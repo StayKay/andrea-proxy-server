79816382d622fb2df2a9404dbca805a858ea49e5
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-03-19 10:29:38 -0700

    set up serving availability services

--- index.js
+++ index.js
@@ -15,6 +15,33 @@ app.get("/api/reviews/:locationId", async (req, res) => {
   res.end();
 });
 
+app.get("/api/reserve/:locationId", async (req, res) => {
+  const { locationId } = req.params;
+  await axios
+    .get(`http://localhost:3002/api/reserve/${locationId}`)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
+  res.end();
+});
+
+app.get("/api/reserve/dates/:check:out", async (req, res) => {
+  const { out } = req.params;
+  await axios
+    .get(`http://localhost:3002/api/reserve/dates/${out}`)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+// app.post("/api/reserve/book/:locationId", async (req, res) => {}); // idk how to do this one
+
 app.listen(4040, () => {
   console.log("Listening on 4040");
 });
